f06393c8704b6cf10a5cae71416e3f237dd490a2
Jesus Francisco Serna Monsivais <sernafrancisco850@gmail.com>
2023-07-25 16:27:21 -0600

    Correccion Catch

--- js/app.js
+++ js/app.js
@@ -50,5 +50,11 @@ fetch(url)
 
   </div>
   `;
- })
+ }).catch(() => {
+  if (countryName.length == 0) {
+    result.innerHTML = `<h3 class='error-msg'>The input field cannot be empty</h3>`;
+  } else {
+    result.innerHTML = `<h3 class='error-msg'>Please enter a valid country name.</h3>`;
+  }
+});
 });
