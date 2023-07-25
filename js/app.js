3e8c02c04dee89bbc69c9da65f0c35e70bf0f4c0
Jesus Francisco Serna Monsivais <sernafrancisco850@gmail.com>
2023-07-25 16:05:38 -0600

    Funcionalidad de Boton y Renderizado en html

--- js/app.js
+++ js/app.js
@@ -8,14 +8,13 @@ const countryInput = document.getElementById('country-input');
 searchButton.addEventListener('click', () => {
 // Input Value...
 let countryName = countryInput.value;
-// let countryName = 'mexico';
 let url = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`;
 console.log(url);
 
 fetch(url)
 .then(res => res.json())
 .then(data => {
-  // console.log(data[0]);
+  console.log(data[0]);
   // console.log(data[0].capital[0]);
   // console.log(data[0].flags.svg);
   // console.log(data[0].name.common);
@@ -26,8 +25,32 @@ fetch(url)
 
 
   result.innerHTML = `
-  
-  
+  <img src="${data[0].flags.svg}" class="flag-country"/>
+
+  <div class='content-country'> 
+
+  <div class='data-capital'>
+  <p class='data-display'><span>Capital:</span> ${data[0].capital[0]}</p>
+  </div>
+
+  <div class='data-continent'>
+  <p class='data-display'><span>Continent:</span> ${data[0].continents[0]}</p>
+  </div>
+
+  <div class='data-population'>
+  <p class='data-display'><span>Population:</span> ${data[0].population}</p>
+  </div>
+
+  <div class='data-population'>
+  <p class='data-display'><span>Currency:</span> ${data[0].currencies[Object.keys(data[0].currencies)].name}</p>
+  </div>
+
+  <div class='data-population'>
+  <p class='data-display'><span>Common Languages:</span> pENE</p>
+  </div>
+
+
+  </div>
   `;
  })
 });
