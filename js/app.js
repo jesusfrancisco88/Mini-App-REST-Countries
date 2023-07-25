3cabd91919959aee656473db5cf9d053015b7c77
Jesus Francisco Serna Monsivais <sernafrancisco850@gmail.com>
2023-07-25 16:17:02 -0600

    Responsive

--- js/app.js
+++ js/app.js
@@ -4,7 +4,6 @@ const searchButton = document.getElementById('search-btn');
 const countryInput = document.getElementById('country-input');
 
 
-
 searchButton.addEventListener('click', () => {
 // Input Value...
 let countryName = countryInput.value;
@@ -23,30 +22,29 @@ fetch(url)
   // console.log(data[0].currencies[Object.keys(data[0].currencies)].name);
   // console.log(Object.values(data[0].languages).toString());
 
-
   result.innerHTML = `
   <img src="${data[0].flags.svg}" class="flag-country"/>
 
   <div class='content-country'> 
 
   <div class='data-capital'>
-  <p class='data-display'><span>Capital:</span> ${data[0].capital[0]}</p>
+  <p class='data-display'><span>Capital: </span> ${data[0].capital[0]}</p>
   </div>
 
   <div class='data-continent'>
-  <p class='data-display'><span>Continent:</span> ${data[0].continents[0]}</p>
+  <p class='data-display'><span>Continent: </span> ${data[0].continents[0]}</p>
   </div>
 
   <div class='data-population'>
-  <p class='data-display'><span>Population:</span> ${data[0].population}</p>
+  <p class='data-display'><span>Language: ${Object.values(data[0].languages).toString()}</span></p>
   </div>
 
   <div class='data-population'>
-  <p class='data-display'><span>Currency:</span> ${data[0].currencies[Object.keys(data[0].currencies)].name}</p>
+  <p class='data-display'><span>Currency: </span>${data[0].currencies[Object.keys(data[0].currencies)].name}</p>
   </div>
 
   <div class='data-population'>
-  <p class='data-display'><span>Common Languages:</span> pENE</p>
+  <p class='data-display'><span>Population: </span> ${data[0].population}</p>
   </div>
 
 
